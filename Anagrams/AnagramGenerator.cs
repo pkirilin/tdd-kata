07d8af3f253d3f8f30bee2513b1520cab3cb5674
Pavel Kirilin <p.kirilin@dodopizza.com>
2021-01-25 15:35:59 +0300

    Fixed green test

--- Anagrams/AnagramGenerator.cs
+++ Anagrams/AnagramGenerator.cs
@@ -14,9 +14,14 @@ namespace Anagrams
 
             if (input.Length == 2)
             {
+                if (input.ElementAt(0) == input.ElementAt(1))
+                {
+                    return new[] { input };
+                }
+                
                 return new[] { input, string.Join("", input.Reverse()) };
             }
-            
+
             return Array.Empty<string>();
         }
     }
