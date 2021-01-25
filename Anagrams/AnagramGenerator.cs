953ccb5ffee9829d623aff43ceb264c0d61accc9
Anatoly Sorokin <a.sorokin@dodopizza.com>
2021-01-25 16:34:19 +0300

    add WhenGenerateWithStringWhichContainsFourSymbols_ThenReturnsTwentyFourPermutations

--- Anagrams/AnagramGenerator.cs
+++ Anagrams/AnagramGenerator.cs
@@ -7,6 +7,17 @@ namespace Anagrams
     {
         public static string[] Execute(string input)
         {
+            // TODO: add test
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+            
+            if (input.Length == 0)
+            {
+                return Array.Empty<string>();
+            }
+            
             if (input.Length == 1)
             {
                 return new[] { input.ElementAt(0).ToString() };
@@ -14,38 +25,34 @@ namespace Anagrams
 
             if (input.Length == 2)
             {
-                return SimpleCase(input[0], input[1]);
+                return GetOutputTails(new[] { input[0], input[1] });
             }
 
-            if (input.Length > 2)
-            {
-                // idx -> 0, 1, 2
-                // input[curIdx]
-
-                var one = ComplexCase(input[0], input.Where(x => x != input[0]).ToArray());
-                var two = ComplexCase(input[1], input.Where(x => x != input[1]).ToArray());
-                var three = ComplexCase(input[2], input.Where(x => x != input[2]).ToArray());
-                return one.Concat(two).Concat(three).ToArray();
-            }
-
-            return Array.Empty<string>();
+            return input
+                .SelectMany(x => ComplexCase(x, input.Where(y => x != y).ToArray()))
+                .ToArray();
         }
-
-        private static string[] ComplexCase(char x1, char[] tail)
+        
+        private static string[] ComplexCase(char head, char[] tail)
         {
-            return SimpleCase(tail[0], tail[1])
-                .Select(y => $"{x1}{y}")
+            if (tail.Length == 3)
+            {
+                var tempTails = tail.SelectMany(head => ComplexCase(head, tail.Where(y => y != head).ToArray()));
+                return tempTails
+                    .Select(y => $"{head}{y}")
+                    .ToArray();
+            }
+            
+            return GetOutputTails(tail)
+                .Select(y => $"{head}{y}")
                 .ToArray();
         }
 
-        private static string[] SimpleCase(char x1, char x2)
+        private static string[] GetOutputTails(char[] tail)
         {
-            if (x1 == x2)
-            {
-                return new[] { $"{x1}{x2}" };
-            }
-            
-            return new[] { $"{x1}{x2}", $"{x2}{x1}" };
+            return tail[0] == tail[1]
+                ? new[] {$"{tail[0]}{tail[1]}"}
+                : new[] {$"{tail[0]}{tail[1]}", $"{tail[1]}{tail[0]}"};
         }
     }
 }
