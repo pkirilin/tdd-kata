0cf55ae5753688356ce19faf30c3de4ef2c05f4f
Pavel Kirilin <p.kirilin@dodopizza.com>
2021-01-26 10:49:56 +0300

    Refactoring

--- Anagrams/AnagramGenerator.cs
+++ Anagrams/AnagramGenerator.cs
@@ -8,40 +8,30 @@ namespace Anagrams
         public static string[] Execute(string input)
         {
             // TODO: add test
-            if (input == null)
-            {
-                throw new ArgumentNullException(nameof(input));
-            }
-            
-            if (input.Length == 0)
+            if (string.IsNullOrWhiteSpace(input))
             {
                 return Array.Empty<string>();
             }
             
-            if (input.Length == 1)
-            {
-                return new[] { input.ElementAt(0).ToString() };
-            }
-
-            if (input.Length == 2)
-            {
-                return GetOutputTails(new[] { input[0], input[1] });
-            }
-
             return input
                 .SelectMany(x => ComplexCase(x, input.Where(y => x != y).ToArray()))
+                // .SelectMany(x => ComplexCase(x, input.ToArray()))
+                // .Distinct()
                 .ToArray();
         }
         
         private static string[] ComplexCase(char head, char[] tail)
         {
-            if (tail.Length == 2)
+            if (tail.Length == 0)
             {
-                return GetOutputTails(tail)
-                    .Select(x => $"{head}{x}")
-                    .ToArray();
+                return new[] { head.ToString() };
             }
             
+            if (tail.Length == 1)
+            {
+                return GetOutputTails(new[] { head, tail[0] });
+            }
+
             var tempTails = tail
                 .SelectMany(h => ComplexCase(h, tail.Where(y => y != h).ToArray()))
                 .ToArray();
@@ -52,9 +42,12 @@ namespace Anagrams
 
         private static string[] GetOutputTails(char[] tail)
         {
-            return tail[0] == tail[1]
-                ? new[] {$"{tail[0]}{tail[1]}"}
-                : new[] {$"{tail[0]}{tail[1]}", $"{tail[1]}{tail[0]}"};
+            if (tail[0] == tail[1])
+            {
+                return new[] { tail[0].ToString() };
+            }
+            
+            return new[] {$"{tail[0]}{tail[1]}"};
         }
     }
 }
