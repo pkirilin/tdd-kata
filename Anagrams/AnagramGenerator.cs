b09e2f5d5f2348115b64d51c99605a94570e6608
Anatoly Sorokin <a.sorokin@dodopizza.com>
2021-01-26 13:13:51 +0300

    remove ExecuteInternal

--- Anagrams/AnagramGenerator.cs
+++ Anagrams/AnagramGenerator.cs
@@ -16,49 +16,27 @@ namespace Anagrams
             {
                 return new[] {input};
             }
-            
-            // return ExecuteInternal(input)
-            //     .Distinct()
-            //     .ToArray();
 
-            // return ExecuteInternal(" " + input)
-            //     .Select(x => x.Remove(0, 1))
-            //     .Distinct()
-            //     .ToArray();
-
-            return GetInputVariations(input)
-                .SelectMany(ExecuteInternal)
-                .Distinct()
-                .ToArray();
-        }
-
-        private static string[] ExecuteInternal(string input)
-        {
-            var head = input.First();
-            var tail = new string(input.Skip(1).ToArray());
-            
-            if (tail.Length == 0)
+            if (input.Length == 2)
             {
-                return new[] {head.ToString()};
-            }
-            
-            if (tail.Length == 1)
-            {
-                if (head == tail[0])
+                if (input[0] == input[1])
                 {
-                    return new[] { $"{head}{tail[0]}" };
+                    return new[] {$"{input[0]}{input[1]}"};
                 }
-                
+
                 return new[]
                 {
-                    $"{head}{tail[0]}",
-                    $"{tail[0]}{head}"
+                    $"{input[0]}{input[1]}",
+                    $"{input[1]}{input[0]}"
                 };
             }
 
-            return GetInputVariations(tail)
-                .SelectMany(ExecuteInternal)
-                .SelectMany(t => new[] { $"{head}{t}" })
+            return GetInputVariations(input)
+                .SelectMany(variation =>
+                    GetInputVariations(new string(variation.Skip(1).ToArray()))
+                        .SelectMany(Execute)
+                        .SelectMany(tail => new[] {$"{variation.First()}{tail}"}))
+                .Distinct()
                 .ToArray();
         }
 
@@ -67,21 +45,21 @@ namespace Anagrams
             var variations = new string[source.Length];
             variations[0] = source;
             var idx = 1;
-            
+
             var currentVariation = source;
             var sourceSymbols = currentVariation.ToArray();
-            
+
             while (idx < source.Length)
             {
                 var head = sourceSymbols[0];
                 var tail = currentVariation.Skip(1);
-                var variation = new string(tail.Concat(new[] { head }).ToArray());
+                var variation = new string(tail.Concat(new[] {head}).ToArray());
                 variations[idx++] = variation;
-            
+
                 currentVariation = variation;
                 sourceSymbols = currentVariation.ToArray();
             }
-            
+
             return variations;
         }
     }
