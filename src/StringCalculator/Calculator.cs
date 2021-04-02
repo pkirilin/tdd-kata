7ab1eac8346a7347bfbc92f60c2c66a803012bc2
Pavel Kirilin <p.kirilin@dodopizza.com>
2021-04-02 18:07:12 +0300

    String calculator refactor: renamed variables

--- src/StringCalculator/Calculator.cs
+++ src/StringCalculator/Calculator.cs
@@ -6,43 +6,41 @@ namespace StringCalculator
 {
     public static class Calculator
     {
-        public static int Add(string numbers)
+        public static int Add(string inputString)
         {
-            if (string.IsNullOrWhiteSpace(numbers))
+            if (string.IsNullOrWhiteSpace(inputString))
                 return 0;
 
             var delimiters = new List<string>() {",", "\n"};
-            var numbersData = numbers;
+            var numbersString = string.Copy(inputString);
             
-            if (numbers.HasCustomDelimiters())
+            if (inputString.HasCustomDelimiters())
             {
-                var customDelimiters = numbers.GetCustomDelimiters();
+                var customDelimiters = inputString.GetCustomDelimiters();
                 delimiters.AddRange(customDelimiters);
-                numbersData = numbers.RemoveFirstLine();
+                numbersString = inputString.RemoveFirstLine();
             }
 
-            var numbersToSum = numbersData.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries)
+            var numbers = numbersString.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .Where(n => n <= 1000)
                 .ToArray();
-            var negativeNumbers = numbersToSum.Where(n => n < 0).ToArray();
+            var negativeNumbers = numbers.Where(n => n < 0).ToArray();
 
             if (negativeNumbers.Any())
-            {
                 throw new InvalidOperationException($"negatives not allowed: {string.Join(", ", negativeNumbers)}");
-            }
 
-            return numbersToSum.Sum();
+            return numbers.Sum();
         }
 
-        private static bool HasCustomDelimiters(this string numbers)
+        private static bool HasCustomDelimiters(this string str)
         {
-            return numbers.StartsWith("//");
+            return str.StartsWith("//");
         }
 
-        private static IEnumerable<string> GetCustomDelimiters(this string numbers)
+        private static IEnumerable<string> GetCustomDelimiters(this string str)
         {
-            var customDelimitersLine = new string(numbers.Skip(2)
+            var customDelimitersLine = new string(str.Skip(2)
                 .TakeWhile(ch => ch != '\n')
                 .ToArray());
             
